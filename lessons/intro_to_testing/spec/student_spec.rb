40dc5d579c6a74f720d105e2722c22cf1db8fa18
Jim Macur <jim.macur@gmail.com>
2024-07-02 16:37:02 -0600

    First Rspec work

--- lessons/intro_to_testing/spec/student_spec.rb
+++ lessons/intro_to_testing/spec/student_spec.rb
@@ -1,13 +1,32 @@
+require "./lib/student"
+require "rspec"
 # Add the 2 requires you will need here
 
 
 describe Student do
   describe '#initialize' do
     it 'is an instance of student' do
+      student = Student.new("Jim")
+      expect(student).to be_a Student
       # write the code to initialize a new student object
     end
-    # test it has a name
-    # test it has cookies
+    
+    it 'has a name' do
+      student = Student.new("Jim")
+      expect(student.name).to eq("Jim")
+    end
+
+    it 'has cookies' do
+      student = Student.new("Jim")
+      expect(student.cookies).to eq []
+    end
+
+    it 'can add cookies' do
+      student = Student.new("Jim")
+      student.add_cookie("Chocolate Chip")
+      student.add_cookie("Thin Mint")
+      expect(student.cookies).to eq ["Chocolate Chip", "Thin Mint"]
+    end
     # test it can add cookies
   end
 end
