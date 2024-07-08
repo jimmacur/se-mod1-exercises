78dd4f58e47ebe0688ebf9661344fb6e3ab31ffd
Jim Macur <jim.macur@gmail.com>
2024-07-07 20:53:39 -0600

    Completes vanue file

--- practice_independent_challenges/week1/venue/lib/venue.rb
+++ practice_independent_challenges/week1/venue/lib/venue.rb
@@ -1,2 +1,18 @@
 class Venue
+    attr_reader :name, :capacity, :patrons
+
+    def initialize(name, capacity)
+        @name = name
+        @capacity = capacity
+        @patrons = []
+    end
+
+    def add_patron(patron_name)
+        #put the patron_name into the patrons array
+        @patrons.push(patron_name)
+    end
+
+    def yell_at_patrons
+        @patrons.map(&:upcase)
+    end
 end
