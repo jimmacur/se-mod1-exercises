6fbbd5b42a2bc9d857e12763954dfebdafd9dd24
Jim Macur <jim.macur@gmail.com>
2024-07-20 17:21:20 -0600

    test: add players

--- practice_independent_challenges/week3/world_cup/lib/team.rb
+++ practice_independent_challenges/week3/world_cup/lib/team.rb
@@ -1,7 +1,17 @@
 class Team
-    attr_reader :country
+    attr_reader :country, :players
 
     def initialize(country)
+        @country = country
+        @eliminated = false
+        @player = []
+    end
 
+    def eliminated?
+        @eliminated
+    end
+
+    def eliminated
+        @eliminated = true
     end
 end
--- practice_independent_challenges/week3/world_cup/spec/team_spec.rb
+++ practice_independent_challenges/week3/world_cup/spec/team_spec.rb
@@ -4,11 +4,34 @@ require './lib/team'
 RSpec.describe Team do
     before(:each) do
         @team = Team.new("France")
+        @mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+        @pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
     end
 
-    describe 'initialize' do
+    describe '#initialize' do
         it 'exists' do
             expect(@team).to be_an_instance_of Team
+        end
+    end
+
+    describe '#attributes' do
+        it 'defaults to false - eliminated' do
+            expect(@team.eliminated?).to be false
+        end
+
+        it 'can change eliminated to true' do
+            @team.eliminated
+
+            expect(@team.eliminated?).to be true
+        end
+    end
+
+    describe '#add_players' do
+        it 'can add players' do
+            @team.add_player(@mbappe)
+            @team.add_player(@pogba)
+
+            expect(@team.players).to eq [@mbappe, @pogba]
         end
     end
 end
