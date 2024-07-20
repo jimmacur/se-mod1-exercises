8a364126144c8d7a0e755d1430ebf49b1d4ca51f
Jim Macur <jim.macur@gmail.com>
2024-07-20 17:04:32 -0600

    Initial commit

---
+++ practice_independent_challenges/week3/world_cup/spec/player_spec.rb
@@ -0,0 +1,20 @@
+require './lib/player'
+
+RSpec.describe Player do
+    before(:each) do
+        @player = Player.new({name: "Luka Modric", position: "midfielder"})    
+    end
+
+    describe 'instantiate' do
+        it 'can exist' do
+            expect(@player).to be_an_instance_of Player
+        end
+
+        it ' has attributes' do
+            expect(@player.name).to eq "Luka Modric"
+            expect(@player.position).to eq "midfielder"
+        end
+    end
+
+
+end
