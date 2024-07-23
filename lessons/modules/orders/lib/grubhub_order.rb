7f2624cad4552700c93853b7d200a5952fd6e4ef
Jim Macur <jim.macur@gmail.com>
2024-07-23 10:44:10 -0600

    modules

--- lessons/modules/orders/lib/grubhub_order.rb
+++ lessons/modules/orders/lib/grubhub_order.rb
@@ -1,14 +1,17 @@
+require './lib/deliverable'
+
 # ./grubhub_order.rb
 class GrubhubOrder
-  def confirmation(thing)
-    "You got #{thing}."
-  end
+  include Deliverable
+  # def confirmation(thing)
+  #   "You got #{thing}."
+  # end
 
   def delivery
     "Your food will arrive in 45-60 minutes."
   end
 
-  def review
-    "Please rate your order within 30 days."
-  end
+  # def review
+  #   "Please rate your order within 30 days."
+  # end
 end
